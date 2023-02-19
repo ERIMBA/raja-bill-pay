f76ce02cda3b88139b003c88c547ca0d86826aa5
ERIMBA <94919980+ERIMBA@users.noreply.github.com>
2023-02-19 09:31:32 +0300

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Main.css">
+    
+    <title>Raja Bill Pay</title>
+</head>
+<body>
+    <div class="header"> 
+       <h1> RAJA BILL PAY </h1>
+       <h2>PayBill: 923737</h2>
+       <p><img src="Images\saf1.jpg" alt="icon" class="img1">
+       <img src="Images\airtel.jpg" alt="icon" class="img2">
+       <img src="Images\telkom.jpg" alt="icon" class="img3">
+      
+      </p>
+<hr> </div>
+  
+<div class="container size">
+<h4>You can pay your bills and airtime to all networks Safaricom, Airtel, Faiba and Telkom even with fuliza, we got you covered with our paybill number.
+Follow the instructions given below to pay your bills and airtime.</h4>
+
+<h2>How to Top Up Procedure Through Mpesa:</h2>
+<ol>
+    <li>Go to Mpesa</li>
+    <li>Lipa Na Mpesa</li> 
+    <li>PayBill number</li>
+    <li>Enter Business Number:<h3>PayBill: 923737</h3></li>
+    <li>Enter you number as account number</li>
+    <li>Enter Amount</li>
+     <li>Enter pin to confirm</li>
+
+</ol>
+<hr>
+
+
+</div>
+
+    
+    <footer class="footer">
+
+    <div class="row">
+  <div class="column" style="background-color:#ccc;">
+    <h3>About Us</h3>
+    <p>Raja Bill pay is a technology based<br>
+      enterprice that provides easy and fastest<br> 
+      way to pay your bills and purchaning airtime<br>
+      for all the networks.
+    </p>
+  </div>
+  <div class="column" style="background-color:#ccc;">
+    <h3>Contact Us</h3>
+    <p>You can reach out to us through:</p>
+    <div>Mobile: 0711122233</div>
+    <div</div>
+  </div>
+  <div class="column" style="background-color:#ccc;">
+    <h3>Social Media Handles</h3>
+    <p>Some text..</p>
+  </div>
+</div>
+<div class="foo">All rights preserved @2023</div>
+    </footer>
+
+     
+</body>
+</html>
